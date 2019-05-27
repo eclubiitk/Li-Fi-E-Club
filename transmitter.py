4badaf9b7e3d657e9aac565e3005fdcb300facf1
ramandeep11 <50864818+ramandeep11@users.noreply.github.com>
2019-05-27 23:50:54 +0530

    arduino

---
+++ transmitter.py
@@ -0,0 +1,25 @@
+string ="g"#input("")#justchange input
+string2=""
+for x in string:
+    i=ord(x)
+    print(i)
+    string2+=bin(i)
+
+
+string3=""
+for x in string2:
+    if x=="b":
+        continue
+    else:
+        string3+=x
+
+string2=""
+#manchester incoding starts
+for x in string3:
+    if x=="0":
+        string2+="10"
+    else:
+        string2+="01"
+
+print(string2)#comment this its for checking only
+print(string3)
