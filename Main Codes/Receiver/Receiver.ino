78ae4c82ddf62adf80739dd0191036df23aef48b
Utkarsh Gupta <utkarshg99@gmail.com>
2019-07-07 22:42:49 +0530

    rec-trans ard

---
+++ Main Codes/Receiver/Receiver.ino
@@ -0,0 +1,25 @@
+int pin = 3;
+unsigned long duration;
+
+void setup() {
+  Serial.begin(230400);
+  pinMode(pin, INPUT);
+}
+
+int flag = 1 ;
+void loop() {
+  duration = pulseIn(pin, HIGH);
+  if(135 < duration && duration < 165) {
+    if(flag == 1) {
+      Serial.println(1) ;
+      flag = 0 ;
+    }
+    else {
+      flag = 1 ;
+    }
+  }
+  else {
+    Serial.println(0) ;
+  }
+  // Serial.println(duration);
+}
